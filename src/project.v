a310d1cf30bd7c681c0295262f17216aca0cad0f
Salieri <70080846+Bondrewd117@users.noreply.github.com>
2025-08-27 13:18:03 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,11 +17,24 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_oe  = 8'b0000_0000;  // Set all IOs as inputs
+  assign uio_out = 8'b0000_0000;  // Drive outputs to 0 when not used
+  assign uio_in = 8'b0000_0000; 
+  wire enable;
+  wire [7:0] c;
+
+  assign enable  = ui_in[0];  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[7:0] = c;
+
+  SUMADOR8BITS SUMADOR8BITS_Unit(
+        .clk(clk),
+        .rst(rst_n),
+        .enable(enable), 
+        .count(c)
+    );
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in[7:0], uio_out[7:0], ui_in[7:1], 1'b0};
+
 
 endmodule
