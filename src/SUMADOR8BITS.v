3729f290690ef0bcac23b7ea47bdfcf20667141b
Salieri <70080846+Bondrewd117@users.noreply.github.com>
2025-08-27 13:42:35 -0600

    f1 SUMADOR8BITS.v

--- src/SUMADOR8BITS.v
+++ src/SUMADOR8BITS.v
@@ -2,13 +2,13 @@ module SUMADOR8BITS(
 	input clk,
 	input rst,
 	input enable,
-	ouput reg [7:0] c
+	output reg [7:0] c
 );
 
 	always@(posedge clk or negedge rst) begin
 		if(!rst)
 			c=0;
-		else if(en)
+		else if(enable)
 			c=c+1;
 	end
 endmodule
